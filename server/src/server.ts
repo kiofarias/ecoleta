e12051c09436a2d091a5c0caf89788d8404ad600
Francisco Farias <kiofarias@gmail.com>
2020-08-08 23:43:02 -0300

    database created

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,8 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
-
-app.get('/users',(req,res)=>{
-    console.log('Listagem de usuários')
-    res.json([
-        'Francisco',
-        'Marina'
-    ]);
-});
+app.use(express.json());
+app.use(routes); 
 
 app.listen(3333);
